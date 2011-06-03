110bcba3e235d2047d79491e45709792df66749f
Laurent Ren <ysaiph.core@gmail.com>
2011-06-03 12:15:37 +0200

    application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0afdc519d8177dca6d31d62863761420769cb1a4fd3000fd17c74da7803f4fb34bbcea50ffbd04b31a9b90c056bce622b54ecdf3ae533bf271fdf531cd472793'
